ba3770ed6b60e30083c05e11cdf109f23e832bc2
Sakharuk Viktar <natgeogr93@gmail.com>
2020-10-01 16:39:27 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arrFromString = str.split('');
+
+  if (arrFromString.length % 2 !== 0) return false;
+    
+  for (let bracket of arrFromString){
+    if (!bracketsConfig.flat().includes(bracket)) return false;
+  }
+  
+  const resultArr = [];
+  
+  arrFromString.forEach(item => {
+    resultArr.push(item);
+    if (resultArr[resultArr.length-2] === openBracket(item) && resultArr.length-2 >= 0){
+      resultArr.pop();
+      resultArr.pop();
+    }   
+  });
+  
+  return (resultArr.length === 0)? true: false;  
+}
+
+function openBracket(char){
+
+  switch (char){
+    case (')'): return '(';
+    case (']'): return '[';
+    case ('}'): return '{';
+    case ('|'): return '|';
+    case ('2'): return '1';
+    case ('4'): return '3';
+    case ('6'): return '5';
+    case ('7'): return '7';
+    case ('8'): return '8';
+  }
 }
